6d02046c1917f6761849a6d0b44decd3fefb0a39
jalen <jiayuewang228@gmail.com>
2012-02-07 13:47:53 +0800

    Delete some dead code

--- config/emacs/org-mode.el
+++ config/emacs/org-mode.el
@@ -117,30 +117,6 @@
    ;;http://emacser.com/org-mode.htm
    (require 'org-publish)
 
-   ;; (setq note-root-dir (concat (getenv "dropbox") "\\home\\notes"))
-
-   ;; (setq note-publish-dir (concat (getenv "dropbox") "\\home\\publish"))
-
-   ;; (setq org-publish-project-alist
-   ;;       `(("note-org"
-   ;;          :base-directory ,note-root-dir
-   ;;          :publishing-directory ,note-publish-dir
-   ;;          :base-extension "org"
-   ;;          :recursive t
-   ;;          :publishing-function org-publish-org-to-html
-   ;;          :auto-index t
-   ;;          :index-filename "index.org"
-   ;;          :index-title "index"
-   ;;          :link-home "index.html")
-   ;;         ("note-static"
-   ;;          :base-directory ,note-root-dir
-   ;;          :publishing-directory ,note-publish-dir
-   ;;          :recursive t
-   ;;          :base-extension "css//|js//|png//|jpg//|gif//|pdf//|mp3//|swf//|zip//|gz//|txt//|el"
-   ;;          :publishing-function org-publish-attachment)
-   ;;         ("note" :components ("note-org" "note-static")
-   ;; 		 )))
-
    (setq org-publish-project-alist
 		 '(("note-org"
 			:base-directory "c:/Users/wangjiay/Documents/My DBank/home/notes"
