9133274f66b8795d6869d2054ef8ae76e3de062c
jalen <jiayuewang228@gmail.com>
2012-02-08 09:23:48 +0800

    suppress the toc when export the org

--- config/emacs/org-mode.el
+++ config/emacs/org-mode.el
@@ -159,7 +159,7 @@
 (setq org-agenda-include-diary nil)
 (setq org-timeline-show-empty-dates t)
 (setq org-insert-mode-line-in-empty-file t)
-(setq org-export-with-toc nil)
+;;(setq org-export-with-toc nil)
 
 
 ;;http://www.cnblogs.com/doveyoung/archive/2011/01/12/Emacs-17.html
